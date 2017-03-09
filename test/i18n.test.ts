fe85fa0a2486b72f0e28435fa1759b2ae02ba4f3
PKief <philipp.kief@gmx.de>
2017-03-09 22:19:06 +0100

    removed comments

--- test/i18n.test.ts
+++ test/i18n.test.ts
@@ -1,17 +1,7 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as i18n from '../src/i18n';
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("i18n", () => {
     test("translations can be initialized", () => {
         return i18n.initTranslations();
