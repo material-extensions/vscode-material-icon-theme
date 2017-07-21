95f17edc3adbbb9a0e13f0500907f3b88291341d
PKief <philipp.kief@gmx.de>
2017-07-21 18:11:43 +0200

    minor refactoring

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -52,23 +52,28 @@ export const translate = (key: string, translations = currentTranslation, fallba
 /** Get the nested keys of an object (http://stackoverflow.com/a/6491621/6942210)
  *
  * *This solution is lighter than the lodash get-version and works fine for the translations.* */
-const getValue = (o, s) => {
-    s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-    s = s.replace(/^\./, '');           // strip a leading dot
-    let a = s.split('.');
+const getValue = (obj: any, key: string): string => {
+    // convert indexes to properties
+    key = key.replace(/\[(\w+)\]/g, '.$1');
+
+    // strip a leading dot
+    key = key.replace(/^\./, '');
+
+    // separate keys in array
+    let keyArray = key.split('.');
 
     /** Avoid errors in the getValue function. */
     const isObject = (object) => {
         return object === Object(object);
     };
 
-    for (let i = 0, n = a.length; i < n; ++i) {
-        let k = a[i];
-        if (isObject(o) && k in o) {
-            o = o[k];
+    for (let i = 0; i < keyArray.length; ++i) {
+        let k = keyArray[i];
+        if (isObject(obj) && k in obj) {
+            obj = obj[k];
         } else {
             return;
         }
     }
-    return o;
+    return obj;
 };
