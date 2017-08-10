74404fb0e1b97b444cbfcb481ce900fdfa72600c
Alejandro Barahona <abarahona@paradigmadigital.com>
2017-08-10 14:48:02 +0200

    add I18N spanish

---
+++ src/i18n/lang-es.ts
@@ -0,0 +1,49 @@
+import { Translation } from "../models/Translation.interface";
+
+export const en: Translation = {
+    "themeInstalled": "Material Icon Theme se ha instalado.",
+    "howToActivate": "Cómo activar los iconos",
+    "activate": "Activar",
+    "activated": "Material Icon Theme está activado.",
+    "neverShowAgain": "No mostrar más",
+    "themeUpdated": "Material Icon Theme se ha actualizado.",
+    "readChangelog": "Leer changelog",
+    "angular": {
+        "alreadyEnabled": "Iconos de Angular estan activados.",
+        "alreadyDisabled": "Iconos de Angular están desactivados.",
+        "enableIcons": "Activar iconos de Angular",
+        "disableIcons": "Desactivar iconos de Angular",
+        "toggleIcons": "Cambiar activaciós de iconos de Angular"
+    },
+    "folders": {
+        "alreadyEnabled": "Iconos de Carpetas estan activados.",
+        "alreadyDisabled": "Iconos de Carpetas están desactivados.",
+        "enableIcons": "Activar iconos de Carpetas",
+        "disableIcons": "Desactivar iconos de Carpetas",
+        "toggleIcons": "Cambiar activaciós de iconos de Carpetas",
+        "specific": {
+            "name": "Específico",
+            "description": "Iconos de carpeta específicos"
+        },
+        "classic": {
+            "name": "Clásico",
+            "description": "Iconos de carpeta clásicos"
+        },
+        "blue": {
+            "name": "Azul",
+            "description": "Iconos de carpeta Azul"
+        },
+        "none": {
+            "name": "Ninguno",
+            "description": "Si iconos de carpeta"
+        }
+    },
+    "toggleSwitch": {
+        "on": "ON",
+        "off": "OFF"
+    },
+    "confirmReload": "Debe reiniciar VS Code para activar los cambios en los iconos.",
+    "reload": "Reiniciar",
+    "outdatedVersion": "Debe actualizar VS Code para utilizar este comando.",
+    "updateVSCode": "Actualizar VS Code"
+};
