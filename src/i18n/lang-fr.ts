33b386d4dbeaed798d46a65abdbda9ff93d26fc4
Yanik Crépeau <ycrepeau@me.com>
2017-08-14 19:57:26 +0200

    Make File icon Ruby on Rails Friendly + I18N French #107

---
+++ src/i18n/lang-fr.ts
@@ -0,0 +1,49 @@
+import { Translation } from "../models/Translation.interface";
+
+export const en: Translation = {
+    "themeInstalled": "Material Icon Theme a été installé.",
+    "howToActivate": "Comment activer les icônes",
+    "activate": "Activer",
+    "activated": "Material Icon Theme est actif.",
+    "neverShowAgain": "Ne plus afficher",
+    "themeUpdated": "Material Icon Theme a été mis à jour.",
+    "readChangelog": "Lire la liste des changements",
+    "angular": {
+        "alreadyEnabled": "Les icônes Angular sont déjà actifs.",
+        "alreadyDisabled": "Les icônes Angular sont déjà inactifs.",
+        "enableIcons": "Activer les icônes Angular",
+        "disableIcons": "Désactiver les icônes Angular",
+        "toggleIcons": "Basculer les icônes Angular"
+    },
+    "folders": {
+        "alreadyEnabled": "Les icônes de dossiers sont déjà actifs.",
+        "alreadyDisabled": "Les icônes de dossiers sont déjà inactifs.",
+        "enableIcons": "Activer les icônes de dossiers",
+        "disableIcons": "Désactiver les icônes de dossiers",
+        "toggleIcons": "Basculer les icônes de dossiers",
+        "specific": {
+            "name": "Spécifique",
+            "description": "Icônes de dossiers spécifiques"
+        },
+        "classic": {
+            "name": "Classique",
+            "description": "Icônes de dossiers grises"
+        },
+        "blue": {
+            "name": "Bleu",
+            "description": "Icônes de dossiers bleues"
+        },
+        "none": {
+            "name": "Aucun",
+            "description": "Aucune icônes de dossiers"
+        }
+    },
+    "toggleSwitch": {
+        "on": "ON",
+        "off": "OFF"
+    },
+    "confirmReload": "Veuillez redémarrer VS Code pour activer les icônes",
+    "reload": "Redémarrer",
+    "outdatedVersion": "Vous devez mettre VS Code à jour pour utiliser cette commande.",
+    "updateVSCode": "Mettre VS Code à jour."
+};
