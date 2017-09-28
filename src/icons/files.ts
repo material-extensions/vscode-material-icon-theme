b309a0f67a7def4d7e52aa13b6ea77810b6f6864
PKief <philipp.kief@gmx.de>
2017-09-28 22:44:24 +0200

    added light icons to generator

--- src/icons/files.ts
+++ src/icons/files.ts
@@ -1,4 +1,4 @@
-import { FileIcons, IconGroup } from '../models/index';
+import { FileIcons, IconGroup, IconConfiguration } from '../models/index';
 import { iconFolderPath } from './index';
 
 /**
@@ -10,7 +10,8 @@ export const fileIcons: FileIcons = {
         { name: 'angular', fileNames: ['.angular-cli.json', 'angular-cli.json'] },
         { name: 'javascript', extensions: ['js'] },
         { name: 'angular.component', extensions: ['component.ts', 'component.js'], group: IconGroup.Angular },
-        { name: 'angular.routing', extensions: ['routing.ts', 'routing.js'], group: IconGroup.Angular }
+        { name: 'angular.routing', extensions: ['routing.ts', 'routing.js'], group: IconGroup.Angular },
+        { name: 'stylelint', fileNames: ['.stylelintrc', 'stylelint.config.js', '.stylelintrc.json', '.stylelintrc.yaml', '.stylelintrc.yml', '.stylelintrc.js', '.stylelintignore'], light: true }
     ]
 };
 
@@ -21,21 +22,37 @@ export const getFileIconDefinitions = (): IconConfiguration => {
     let definitions = {
         iconDefinitions: {},
         fileExtensions: {},
-        fileNames: {}
+        fileNames: {},
+        light: {
+            fileExtensions: {},
+            fileNames: {},
+        }
     };
+
     fileIcons.icons.forEach(icon => {
         if (icon.disabled) return;
-        definitions.iconDefinitions[icon.name] = {
+        definitions['iconDefinitions'][icon.name] = {
             iconPath: `${iconFolderPath}${icon.name}.svg`
         };
+        if (icon.light) {
+            definitions['iconDefinitions'][`${icon.name}_light`] = {
+                iconPath: `${iconFolderPath}${icon.name}_light.svg`
+            };
+        }
         if (icon.extensions) {
             icon.extensions.forEach(ext => {
-                definitions.fileExtensions[ext] = icon.name;
+                definitions['fileExtensions'][ext] = icon.name;
+                if (icon.light) {
+                    definitions['light']['fileExtensions'][ext] = `${icon.name}_light`;
+                }
             });
         }
         if (icon.fileNames) {
             icon.fileNames.forEach(fn => {
-                definitions.fileNames[fn] = icon.name;
+                definitions['fileNames'][fn] = icon.name;
+                if (icon.light) {
+                    definitions['light']['fileExtensions'][fn] = `${icon.name}_light`;
+                }
             });
         }
     });
