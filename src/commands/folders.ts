a634c26119826658c38e6a2970c4dd341b6d164e
PKief <philipp.kief@gmx.de>
2017-10-03 23:19:35 +0200

    fixed naming issue

--- src/commands/folders.ts
+++ src/commands/folders.ts
@@ -75,7 +75,7 @@ export const checkFolderIconsStatus = (): Promise<FolderType> => {
             return FolderType.None;
         } else if (config.folderNames && Object.keys(config.folderNames).length > 0) {
             return FolderType.Specific;
-        } else if (config.folder === 'folder_blue') {
+        } else if (config.folder === 'folder-blue') {
             return FolderType.Blue;
         } else {
             return FolderType.Classic;
