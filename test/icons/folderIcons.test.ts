4908b5e1a0d31d42016cc3e9c82bf2695faf1833
PKief <philipp.kief@gmx.de>
2017-10-16 18:31:28 +0200

    added folder test for light and high contrast

--- test/icons/folderIcons.test.ts
+++ test/icons/folderIcons.test.ts
@@ -154,4 +154,111 @@ suite('folder icons', () => {
 
         assert.deepEqual(def, value);
     });
+
+    test('should configure folder icons for light and high contrast', () => {
+        const options = getDefaultIconOptions();
+        const def = getFolderIconDefinitions(folderIcons, iconConfig, options);
+        const value = new IconConfiguration();
+        const lightHighContrastFolderIcons: FolderTheme[] = [
+            {
+                name: 'specific',
+                defaultIcon: { name: 'folder', light: true, highContrast: true },
+                rootFolder: { name: 'folder-root', light: true, highContrast: true },
+                icons: [
+                    { name: 'folder-src', folderNames: ['src', 'source'], light: true, highContrast: true }
+                ]
+            }
+        ];
+
+        value.iconDefinitions = {
+            'folder': {
+                'iconPath': './../../icons/folder.svg'
+            },
+            'folder-open': {
+                'iconPath': './../../icons/folder-open.svg'
+            },
+            'folder_light': {
+                'iconPath': './../../icons/folder_light.svg'
+            },
+            'folder-open_light': {
+                'iconPath': './../../icons/folder-open_light.svg'
+            },
+            'folder_highContrast': {
+                'iconPath': './../../icons/folder_highContrast.svg'
+            },
+            'folder-open_highContrast': {
+                'iconPath': './../../icons/folder-open_highContrast.svg'
+            },
+            'folder-root': {
+                'iconPath': './../../icons/folder-root.svg'
+            },
+            'folder-root-open': {
+                'iconPath': './../../icons/folder-root-open.svg'
+            },
+            'folder-root_light': {
+                'iconPath': './../../icons/folder-root_light.svg'
+            },
+            'folder-root-open_light': {
+                'iconPath': './../../icons/folder-root-open_light.svg'
+            },
+            'folder-root_highContrast': {
+                'iconPath': './../../icons/folder-root_highContrast.svg'
+            },
+            'folder-root-open_highContrast': {
+                'iconPath': './../../icons/folder-root-open_highContrast.svg'
+            },
+            'folder-src': {
+                'iconPath': './../../icons/folder-src.svg'
+            },
+            'folder-src-open': {
+                'iconPath': './../../icons/folder-src-open.svg'
+            },
+            'folder-src_light': {
+                'iconPath': './../../icons/folder-src_light.svg'
+            },
+            'folder-src-open_light': {
+                'iconPath': './../../icons/folder-src-open_light.svg'
+            }
+        };
+        value.folder = 'folder';
+        value.folderExpanded = 'folder-open';
+        value.rootFolder = 'folder-root';
+        value.rootFolderExpanded = 'folder-root-open';
+        value.folderNames = {
+            'src': 'folder-src',
+            'source': 'folder-src'
+        };
+        value.folderNamesExpanded = {
+            'src': 'folder-src-open',
+            'source': 'folder-src-open'
+        };
+        value.light = {
+            folder: 'folder_light',
+            folderExpanded: 'folder-open_light',
+            rootFolder: 'folder-root_light',
+            rootFolderExpanded: 'folder-root-open_light',
+            folderNames: {
+                'src': 'folder-src_light',
+                'source': 'folder-src_light'
+            },
+            folderNamesExpanded: {
+                'src': 'folder-src-open_light',
+                'source': 'folder-src-open_light'
+            }
+        };
+        value.highContrast = {
+            folder: 'folder_highContrast',
+            folderExpanded: 'folder-open_highContrast',
+            rootFolder: 'folder-root_highContrast',
+            rootFolderExpanded: 'folder-root-open_highContrast',
+            folderNames: {
+                'src': 'folder-src_highContrast',
+                'source': 'folder-src_highContrast'
+            },
+            folderNamesExpanded: {
+                'src': 'folder-src-open_highContrast',
+                'source': 'folder-src-open_highContrast'
+            }
+        };
+    });
 });
