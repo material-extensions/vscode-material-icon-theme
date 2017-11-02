aa9788d244bbf2fc12e5b41857b82b9c211fbcd1
Alefe Souza <contact@alefesouza.com>
2017-11-02 18:22:20 -0200

    Added Brazilian Portuguese translation

---
+++ src/i18n/lang-pt-br.ts
@@ -0,0 +1,36 @@
+import { Translation } from '../models';
+
+export const translation: Translation = {
+    'themeInstalled': 'O Material Icon Theme foi instalado.',
+    'howToActivate': 'Como ativar os ícones',
+    'activate': 'Ativar',
+    'activated': 'O Material Icon Theme está ativo.',
+    'neverShowAgain': 'Não mostrar novamente',
+    'themeUpdated': 'O Material Icon Theme foi atualizado.',
+    'readChangelog': 'Ler changelog',
+    'iconPacks': {
+        'selectPack': 'Selecione um pacote de ícones',
+        'description': 'Selecionar o pacote de ícones \'%0\'',
+        'disabled': 'Desabilitar pacotes de ícones',
+    },
+    'folders': {
+        'toggleIcons': 'Escolha um tema para as pastas',
+        'disabled': 'Nenhum ícone de pasta',
+        'theme': {
+            'description': 'Ícones de pasta \'%0\''
+        }
+    },
+    'toggleSwitch': {
+        'on': 'ON',
+        'off': 'OFF'
+    },
+    'hidesExplorerArrows': {
+        'toggleArrows': 'Alternar setas do explorador de arquivos',
+        'enableArrows': 'Exibir setas do explorador de arquivos',
+        'disableArrows': 'Ocultar setas do explorador de arquivos'
+    },
+    'confirmReload': 'Você precisa reiniciar o VS Code para ativar a mudança de ícones.',
+    'reload': 'Reiniciar',
+    'outdatedVersion': 'Você precisa atualizar o VS Code para usar esse comando.',
+    'updateVSCode': 'Atualizar VS Code'
+};
