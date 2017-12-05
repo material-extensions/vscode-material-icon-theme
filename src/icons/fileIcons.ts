ec7fb64a266b9ab9e1d1c39b4fb88bb04fe6b024
Gustavo Reis <gusbemacbe@gmail.com>
2017-12-05 04:45:10 -0200

    Update fileIcons.ts

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -36,6 +36,23 @@ export const fileIcons: FileIcons = {
                 'cdp.pid'
             ]
         },
+        {
+            name: 'jinja',
+            fileExtensions: ['jinja'],
+            fileNames: [
+                'jinja',
+                'jinja2',
+                'j2'
+            ]
+        },
+        {
+            name: 'sublime',
+            fileExtensions: ['sublime'],
+            fileNames: [
+                'sublime-project',
+                'sublime-workspace'
+            ]
+        },
         { name: 'yaml', fileExtensions: ['yaml', 'YAML-tmLanguage', 'yml'] },
         {
             name: 'xml',
