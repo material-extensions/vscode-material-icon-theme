95fe6664afafd9c3cbd146013a481e6c70deb100
PKief <philipp.kief@gmx.de>
2017-12-10 11:02:49 +0100

    added .prettierignore - closes #172

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -601,7 +601,7 @@ export const fileIcons: FileIcons = {
             light: true
         },
         { name: 'code-climate', fileNames: ['.codeclimate.yml'], light: true },
-        { name: 'prettier', fileNames: ['.prettierrc', 'prettier.config.js', '.prettierrc.js', '.prettierrc.json', '.prettierrc.yaml', '.prettierrc.yml'] },
+        { name: 'prettier', fileNames: ['.prettierrc', 'prettier.config.js', '.prettierrc.js', '.prettierrc.json', '.prettierrc.yaml', '.prettierrc.yml', '.prettierignore'] },
         { name: 'nodemon', fileNames: ['nodemon.json'] },
         { name: 'ngrx-reducer', fileExtensions: ['reducer.ts', 'rootReducer.ts'], enabledFor: [IconPack.Ngrx] },
         { name: 'ngrx-state', fileExtensions: ['state.ts'], enabledFor: [IconPack.Ngrx] },
