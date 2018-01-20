faba72a9cfb7a8a0b14674916b33a5b10bbeac84
denisbalyko <denisbalyko@gmail.com>
2018-01-21 00:07:02 +0300

    bugfix: added a file with the language RU

---
+++ src/i18n/lang-ru.ts
@@ -0,0 +1,39 @@
+import { Translation } from '../models';
+
+export const translation: Translation = {
+    'themeInstalled': 'Material Icon Theme был установлен.',
+    'howToActivate': 'Как активировать иконки',
+    'activate': 'Активировать',
+    'activated': 'Material Icon Theme активен.',
+    'neverShowAgain': 'Никогда не показывать снова',
+    'themeUpdated': 'Material Icon Theme был обновлен.',
+    'readChangelog': 'Читать изменения версии',
+    'iconPacks': {
+        'selectPack': 'Выбрать набор иконок',
+        'description': 'Выбрать \'%0\' набор иконок',
+        'disabled': 'Выключить набор иконок',
+    },
+    'folders': {
+        'toggleIcons': 'Выбрать тему папки',
+        'color': 'Выбрать цвет папки',
+        'hexCode': 'Вставить HEX-код цвета',
+        'wrongHexCode': 'Неверный HEX-код цвета!',
+        'disabled': 'Нет иконки для папки',
+        'theme': {
+            'description': 'Выбрать \'%0\' тему папки'
+        }
+    },
+    'toggleSwitch': {
+        'on': 'Включить',
+        'off': 'Выключить'
+    },
+    'hidesExplorerArrows': {
+        'toggleArrows': 'Показать/Скрыть стрелки у папок',
+        'enableArrows': 'Показать стрелки у папок',
+        'disableArrows': 'Скрыть стрелки у папок'
+    },
+    'confirmReload': 'Нужно перезапустить VS Code для активации иконок.',
+    'reload': 'Перезагрузить',
+    'outdatedVersion': 'Нужно обновить VS Code чтобы использовать эту команду.',
+    'updateVSCode': 'Обновить VS Code'
+};
