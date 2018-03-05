b53f307dc871e564d9bdcfbcca94da6970f09f81
PKief <philipp.kief@gmx.de>
2018-03-05 21:33:30 +0100

    Added class folder icon References #112

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -103,6 +103,7 @@ export const folderIcons: FolderTheme[] = [
             { name: 'folder-wordpress', folderNames: ['wp-content'] },
             { name: 'folder-gradle', folderNames: ['gradle', '.gradle'] },
             { name: 'folder-coverage', folderNames: ['coverage'] },
+            { name: 'folder-class', folderNames: ['class', 'classes', 'model', 'models'] },
             { name: 'folder-other', folderNames: ['other', 'others', 'misc', 'miscellaneous'] },
             { name: 'folder-typescript', folderNames: ['typescript', 'ts'] },
         ]
