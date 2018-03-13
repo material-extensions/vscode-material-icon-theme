0c274589763955dc1fdbca249cce7e901a4c3a6c
PKief <philipp.kief@gmx.de>
2018-03-13 23:03:40 +0100

    Fixed issue in change detection

--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,31 @@ import * as vscode from 'vscode';
 import * as i18n from './i18n';
 import * as commands from './commands';
 import { showStartMessages } from './messages/start';
-import { detectConfigChanges, watchForConfigChanges } from './helpers/change-detection';
+import { detectConfigChanges } from './helpers/change-detection';
 import { checkThemeStatus } from './helpers/versioning';
 
+/**
+ * This method is called when the extension is activated.
+ * It initializes the core functionality of the extension.
+ */
 export const activate = (context: vscode.ExtensionContext) => {
+    // Load the translations
     i18n.initTranslations().then(() => {
         showStartMessages(checkThemeStatus(context.globalState));
     }).catch(err => console.error(err));
 
+    // Add commands to the editor
     context.subscriptions.push(...commands.commands);
 
+    // Initially trigger the config change detection
     detectConfigChanges().catch(e => {
         console.error(e);
     });
 
-    watchForConfigChanges();
+    // Observe changes in the config
+    vscode.workspace.onDidChangeConfiguration(detectConfigChanges);
 };
 
+/** This method is called when the extension is deactivated */
 export const deactivate = () => {
 };
--- src/helpers/change-detection.ts
+++ src/helpers/change-detection.ts
@@ -1,13 +1,7 @@
 import * as vscode from 'vscode';
-import * as merge from 'lodash.merge';
 import { createIconFile } from '../icons/index';
 import { getObjectPropertyValue, setObjectPropertyValue } from './objects';
 import { getExtensionConfiguration, promptToReload, getMaterialIconsJSON, getThemeConfig } from '.';
-
-/** Watch for changes in the configurations to update the icons theme. */
-export const watchForConfigChanges = () => {
-    vscode.workspace.onDidChangeConfiguration(detectConfigChanges);
-};
 
 /** Compare the workspace and the user configurations with the current setup of the icons. */
 export const detectConfigChanges = () => {
@@ -16,7 +10,7 @@ export const detectConfigChanges = () => {
 
     return compareConfigs(configs).then(changes => {
         // if there's nothing to update
-        if (Object.keys(changes).length === 0) return;
+        if (!changes) return;
 
         // update icon json file with new options
         return createIconFile(changes).then(() => {
@@ -34,23 +28,19 @@ export const detectConfigChanges = () => {
  * @returns List of configurations that needs to be updated.
  */
 const compareConfigs = (configs: string[]): Promise<{ [name: string]: any }> => {
-    // object with the config properties that must be updated
-    const updateRequired: { [name: string]: any } = {};
+    let updateRequired = false;
 
-    return getMaterialIconsJSON().then((json) => {
+    return getMaterialIconsJSON().then(json => {
         configs.forEach(configName => {
             const configValue = getThemeConfig(configName).globalValue;
             const currentState = getObjectPropertyValue(json.options, configName);
 
             if (configValue !== undefined && JSON.stringify(configValue) !== JSON.stringify(currentState)) {
-                setObjectPropertyValue(updateRequired, configName, configValue);
+                setObjectPropertyValue(json.options, configName, configValue);
+                updateRequired = true;
             }
         });
 
-        if (Object.keys(updateRequired).length > 0) {
-            return merge({}, json.options, updateRequired);
-        } else {
-            return {};
-        }
+        return updateRequired ? json.options : undefined;
     });
 };
