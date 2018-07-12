1d928cd0cfacb534a6cd41bf884c70b77b184b32
rajat <rajat.srivastava@smartprix.com>
2018-07-12 11:34:42 +0530

    jupyter icons

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -719,5 +719,6 @@ export const fileIcons: FileIcons = {
         { name: 'i18n', fileExtensions: ['pot', 'po', 'mo'] },
         { name: 'webassembly', fileExtensions: ['wat', 'wasm'] },
         { name: 'semantic-release', light: true, fileNames: ['.releaserc', 'release.config.js'] },
+        { name: 'jupyter', fileExtensions: ['ipynb'] },
     ]
 };
