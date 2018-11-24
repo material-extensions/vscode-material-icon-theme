61029939c90c8a6115a8bc9db71f408114a1a48d
PKief <philipp.kief@gmx.de>
2018-11-24 10:48:27 +0100

    Updated screenshots script (References #319)

--- scripts/preview/screenshots.ts
+++ scripts/preview/screenshots.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import * as puppeteer from 'puppeteer';
 
 export const createScreenshots = async (filePath: string, fileName: string) => {
-    const htmlFilePath = path.join(__dirname, '..', '..', filePath);
+    const htmlFilePath = path.join('file:', __dirname, '..', '..', filePath);
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -11,7 +11,7 @@ export const createScreenshots = async (filePath: string, fileName: string) => {
         width: 100
     });
 
-    await page.goto(`file:${htmlFilePath}`);
+    await page.goto(htmlFilePath);
 
     await page.screenshot({
         path: `images/${fileName}.png`,
