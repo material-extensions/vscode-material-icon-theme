ee01d66cbae84169ef7c38d4fea722aefee47191
A.R <richardadrien0@gmail.com>
2018-12-21 18:55:28 +0100

    added obj icon

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -771,5 +771,12 @@ export const fileIcons: FileIcons = {
         { name: 'asciidoc', fileExtensions: ['ad', 'adoc', 'asciidoc'] },
         { name: 'istanbul', fileNames: ['.nycrc', '.nycrc.json'] },
         { name: 'edge', fileExtensions: ['edge'] },
+        { 
+            name: 'obj', 
+            fileExtensions: [
+                'stl',
+                'obj'
+            ] 
+        },
     ]
 };
