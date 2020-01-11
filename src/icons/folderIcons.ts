94b578d6edf0db2766610bf5b96563029eadc048
Ed Rands <erands@gmail.com>
2020-01-10 19:50:19 -0800

    Add icons for the Cypress test framework

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -181,6 +181,7 @@ export const folderIcons: FolderTheme[] = [
             { name: 'folder-nuxt', folderNames: ['nuxt', '.nuxt'], enabledFor: [IconPack.Vuex, IconPack.Vue] },
             { name: 'folder-vue-directives', folderNames: ['directives'], enabledFor: [IconPack.Vuex, IconPack.Vue] },
             { name: 'folder-vue', folderNames: ['components'], enabledFor: [IconPack.Vuex, IconPack.Vue] },
+            { name: 'folder-cypress', folderNames: ['cypress'] },
         ]
     },
     { name: 'classic', defaultIcon: { name: 'folder' }, rootFolder: { name: 'folder-root' } },
