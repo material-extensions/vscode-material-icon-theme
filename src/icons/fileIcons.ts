7d3c01781d7225532bc664c715fe8cf9827fad37
Bartłomiej Krakowski <bartlomiej@krakowski.website>
2020-03-04 16:10:20 +0100

    feat(): Add .sc.js and .sc.ts extensions

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -882,6 +882,6 @@ export const fileIcons: FileIcons = {
         { name: 'coconut', fileExtensions: ['coco'] },
         { name: 'capacitor', fileNames: ['capacitor.config.json'] },
         { name: 'sketch', fileExtensions: ['sketch'] },
-        { name: 'styled-components', fileExtensions: ['styled.js', 'styled.ts'] },
+        { name: 'styled-components', fileExtensions: ['styled.js', 'styled.ts', 'sc.js', 'sc.ts'] },
     ]
 };
