b6c065ac1e176f1bc19027c8e8ff999ae1358f9b
Gustavo Cassino <thiefcass@gmail.com>
2020-03-04 20:54:34 -0300

    Add icon to fileicons

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -882,5 +882,8 @@ export const fileIcons: FileIcons = {
         { name: 'coconut', fileExtensions: ['coco'] },
         { name: 'capacitor', fileNames: ['capacitor.config.json'] },
         { name: 'sketch', fileExtensions: ['sketch'] },
+        
+        
+        { name: 'adonisjs', fileExtensions: ['.adonisrc.json'] }
     ]
 };
