f9b7f31c7eabbaae50f664c40895f4d56be0c7aa
lmn8 <60514150+lmn8@users.noreply.github.com>
2020-04-20 21:15:20 +0100

    Add Crowdin extensions & VSCode aliases

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -881,7 +881,7 @@ export const fileIcons: FileIcons = {
         { name: 'husky', fileNames: ['.huskyrc', 'husky.config.js', '.huskyrc.json', '.huskyrc.js', '.huskyrc.yaml', '.huskyrc.yml'] },
         { name: 'coconut', fileExtensions: ['coco'] },
         { name: 'capacitor', fileNames: ['capacitor.config.json'] },
-        { name: 'mediawiki', fileExtensions: ['mediawiki', 'wiki'] },
         { name: 'sketch', fileExtensions: ['sketch'] },
+        { name: 'mediawiki', fileExtensions: ['mediawiki', 'mw', 'wikitext', 'wt', 'wiki'] },
     ]
 };
