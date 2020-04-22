d2633737b2d1a04f0a2c977d3ee1a844b9ab0120
lmn8 <60514150+lmn8@users.noreply.github.com>
2020-04-22 19:05:32 +0100

    Add TypeORM

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -880,5 +880,6 @@ export const fileIcons: FileIcons = {
         { name: 'prolog', fileExtensions: ['p', 'pro'] },
         { name: 'husky', fileNames: ['.huskyrc', 'husky.config.js', '.huskyrc.json', '.huskyrc.js', '.huskyrc.yaml', '.huskyrc.yml'] },
         { name: 'coconut', fileExtensions: ['coco'] },
+        { name: 'typeorm', fileNames: ['ormconfig.json', 'ormconfig.js', 'ormconfig.ts'] }
     ]
 };
