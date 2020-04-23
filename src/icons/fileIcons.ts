7375411dea62c9d5f6b5357c6ed1192a936689bb
lmn8 <60514150+lmn8@users.noreply.github.com>
2020-04-23 19:37:10 +0100

    Switch Pawn icons with SVG (#1)
    * Convert Pawn icon to SVG
    
    * Add light theme Pawn SVG
    
    * Fix conflicting extensions; add 'light' Pawn icon

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -880,6 +880,6 @@ export const fileIcons: FileIcons = {
         { name: 'coconut', fileExtensions: ['coco'] },
         { name: 'capacitor', fileNames: ['capacitor.config.json'] },
         { name: 'sketch', fileExtensions: ['sketch'] },
-        { name: 'pawn', fileExtensions: ['pwn', 'p', 'inc'] },
+        { name: 'pawn', fileExtensions: ['pwn'], light: true },
     ]
 };
