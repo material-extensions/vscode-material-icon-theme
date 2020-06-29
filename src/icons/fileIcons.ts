f737efe5024696f92f478342966363a30ae75508
60514150 <lmn8dev@gmail.com>
2020-06-29 20:16:50 +0100

    Redesign & add light icon

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -903,5 +903,6 @@ export const fileIcons: FileIcons = {
         { name: 'nrwl', fileNames: ['nx.json'] },
         { name: 'imba', fileExtensions: ['imba'] },
         { name: 'drawio', fileExtensions: ['drawio', 'dio'] },
+        { name: 'hjson', fileExtensions: ['hjson']},
     ]
 };
