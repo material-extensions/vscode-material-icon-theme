8abf22b4bf1f80bbc2191efb61393139900f5d54
PKief <philipp.kief@gmx.de>
2020-09-03 11:51:07 +0200

    Add mjml folder icon (Closes #435)

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -189,6 +189,7 @@ export const folderIcons: FolderTheme[] = [
             { name: 'folder-update', folderNames: ['update', 'updates', 'upgrade', 'upgrades'] },
             { name: 'folder-intellij', folderNames: ['.idea'], light: true },
             { name: 'folder-azure-pipelines', folderNames: ['.azure-pipelines', '.azure-pipelines-ci'] },
+            { name: 'folder-mjml', folderNames: ['mjml'] },
         ]
     },
     { name: 'classic', defaultIcon: { name: 'folder' }, rootFolder: { name: 'folder-root' } },
