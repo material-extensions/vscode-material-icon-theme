10cb48f0a37e494d7e8cde6888f2e11fb2f372f4
Abdul Rohman <abdul.rohman84@gmail.com>
2020-10-28 19:12:28 +0700

    add harlow, chapterbook and sugarcube lang support

--- src/icons/languageIcons.ts
+++ src/icons/languageIcons.ts
@@ -117,5 +117,5 @@ export const languageIcons: LanguageIcon[] = [
   { icon: { name: 'search' }, ids: ['search-result'] },
   { icon: { name: 'minecraft' }, ids: ['mcfunction'] },
   { icon: { name: 'rescript' }, ids: ['rescript'] },
-  { icon: { name: 'twine' }, ids: ['twee3'] },
+  { icon: { name: 'twine' }, ids: ['twee3', 'twee3-h3', 'twee3-cb1', 'twee3-sc2'] },
 ];
