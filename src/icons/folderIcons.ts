623e0e355151230c9f6f24e4b995926733f64ab0
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2021-04-14 22:28:37 +0200

    add woocommerce icon

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -602,6 +602,7 @@ export const folderIcons: FolderTheme[] = [
       { name: 'folder-connection', folderNames: ['connection', 'connections'] },
       { name: 'folder-quasar', folderNames: ['.quasar'] },
       { name: 'folder-cobol', folderNames: ['cobol'] },
+      { name: 'folder-woocommerce', folderNames: ['woocommerce'] },
     ],
   },
   {
