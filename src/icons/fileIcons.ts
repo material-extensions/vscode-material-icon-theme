8c4092439ccaa7b3bfb3aabfaa325a4574c3486c
4086606 <82655227+4086606@users.noreply.github.com>
2021-05-22 18:34:52 +0100

    Add webpack icon to `webpack.min.js`

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -725,6 +725,7 @@ export const fileIcons: FileIcons = {
         'webpack.config.dev.ts',
         'webpack.config.dev.babel.js',
         'webpack.config.dev.babel.ts',
+        'webpack.min.js',
         'webpack.prod.js',
         'webpack.prod.config.js',
         'webpack.prod.ts',
