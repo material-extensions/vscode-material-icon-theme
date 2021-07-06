40cf810f2319d52b89f2ccf7f1732141ab0e6ca4
Omri Gabay <omri.gabay@pnmac.com>
2021-07-05 17:30:54 -0700

    Add Rubocop

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -390,6 +390,7 @@ export const fileIcons: FileIcons = {
     { name: 'lib', fileExtensions: ['lib', 'bib'] },
     { name: 'ruby', fileExtensions: ['rb', 'erb'] },
     { name: 'gemfile', fileNames: ['gemfile'] },
+    { name: 'rubocop', fileNames: ['.rubocop.yml', '.rubocop-todo.yml'] },
     { name: 'fsharp', fileExtensions: ['fs', 'fsx', 'fsi', 'fsproj'] },
     { name: 'swift', fileExtensions: ['swift'] },
     { name: 'arduino', fileExtensions: ['ino'] },
