544d6a3b5524a7a5244e42d22402e64064f6a438
Bartłomiej Krakowski <bartlomiej@krakowski.dev>
2021-08-17 10:10:37 +0200

    style: prettier

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -683,8 +683,8 @@ export const fileIcons: FileIcons = {
     { name: 'autoit', fileExtensions: ['au3'] },
     { name: 'haml', fileExtensions: ['haml'] },
     { name: 'yang', fileExtensions: ['yang'] },
-    { 
-      name: 'mjml', 
+    {
+      name: 'mjml',
       fileExtensions: ['mjml'],
       fileNames: ['.mjmlconfig'],
     },
@@ -695,7 +695,7 @@ export const fileIcons: FileIcons = {
     },
     {
       name: 'next',
-      fileNames: [ 'next.config.js', 'next.config.ts' ],
+      fileNames: ['next.config.js', 'next.config.ts'],
       light: true,
     },
     {
