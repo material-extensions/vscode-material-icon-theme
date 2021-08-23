3396968917a8ffabb058a2f662aa739bcf076423
Philipp Kief <philipp.kief@gmx.de>
2021-08-23 22:05:25 +0200

    Code refactoring

--- src/helpers/versioning.ts
+++ src/helpers/versioning.ts
@@ -1,40 +1,45 @@
-import * as semver from 'semver';
-import * as vscode from 'vscode';
-import { isThemeActivated } from '.';
+import { extensions, Memento, version, workspace } from 'vscode';
+// import { isThemeActivated } from '.';
+import { ThemeStatus } from '../models/helpers/themeStatus';
+
+/** Get configuration of vs code. */
+export const getConfig = (section?: string) => {
+  return workspace.getConfiguration(section);
+};
+
+/**
+ * Is the theme already activated in the editor configuration?
+ * @param{boolean} global false by default
+ */
+export const isThemeActivated = (global: boolean = false): boolean => {
+  return global
+    ? getConfig().inspect('workbench.iconTheme').globalValue ===
+        'material-icon-theme'
+    : getConfig().inspect('workbench.iconTheme').workspaceValue ===
+        'material-icon-theme';
+};
 
 export const versionKey = 'material-icon-theme.version';
-
-export enum ThemeStatus {
-  neverUsedBefore,
-  updated,
-  current,
-}
 
 /**
  * Check the current status of the theme
  * @param state Global state of context (Memento API)
  */
-export const checkThemeStatus = async (state: vscode.Memento) => {
+export const checkThemeStatus = async (state: Memento) => {
   try {
     // get the version from the state
     const stateVersion = state.get(versionKey);
     const packageVersion = getCurrentExtensionVersion();
 
     // check if the theme was used before
-    if (
-      stateVersion === undefined ||
-      !(
-        stateVersion instanceof semver.SemVer ||
-        typeof stateVersion === 'string'
-      )
-    ) {
+    if (stateVersion === undefined || typeof stateVersion !== 'string') {
       await updateExtensionVersionInMemento(state);
       return themeIsAlreadyActivated()
         ? ThemeStatus.updated
         : ThemeStatus.neverUsedBefore;
     }
     // compare the version in the state with the package version
-    else if (semver.lt(stateVersion, packageVersion)) {
+    else if (isGreaterVersion(packageVersion, stateVersion)) {
       await updateExtensionVersionInMemento(state);
       return ThemeStatus.updated;
     } else {
@@ -45,19 +50,31 @@ export const checkThemeStatus = async (state: vscode.Memento) => {
   }
 };
 
+/**
+ * Compares two version numbers (e.g. 1.0.0 with 1.0.1)
+ * @param a version b
+ * @param b version a
+ * @returns true if version `a` is greater than version `b`
+ */
+const isGreaterVersion = (a: string, b: string): boolean => {
+  return (
+    a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' }) === 1
+  );
+};
+
 /** Check if the theme was used before */
 const themeIsAlreadyActivated = () => {
   return isThemeActivated() || isThemeActivated(true);
 };
 
 /** Update the version number to the current version in the memento. */
-const updateExtensionVersionInMemento = (state: vscode.Memento) => {
+const updateExtensionVersionInMemento = (state: Memento) => {
   return state.update(versionKey, getCurrentExtensionVersion());
 };
 
 /** Get the current version of the extension */
 const getCurrentExtensionVersion = (): string => {
-  return vscode.extensions.getExtension('PKief.material-icon-theme').packageJSON
+  return extensions.getExtension('PKief.material-icon-theme').packageJSON
     .version;
 };
 
@@ -66,5 +83,5 @@ const getCurrentExtensionVersion = (): string => {
  * supports new features.
  */
 export const checkVersionSupport = (supportedVersion: string): boolean => {
-  return !semver.lt(vscode.version, supportedVersion);
+  return isGreaterVersion(version, supportedVersion);
 };
--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,10 +1,25 @@
 'use strict';
 
+import { ExtensionContext } from 'vscode';
+import { checkThemeStatus, versionKey } from '../helpers/versioning';
+import { showStartMessages } from '../messages/start';
+
 /**
  * This method is called when the extension is activated.
  * It initializes the core functionality of the extension.
  */
-export const activate = async () => {};
+export const activate = async (context: ExtensionContext) => {
+  try {
+    context.globalState.setKeysForSync([versionKey]);
+    const status = await checkThemeStatus(context.globalState);
+    showStartMessages(status);
+
+    // Subscribe to the extension commands
+    context.subscriptions.push();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 /** This method is called when the extension is deactivated */
 export const deactivate = () => {};
