7d96e943e0a513a8e8c1930df4ec9ecc18827795
左丘颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2021-12-13 19:50:23 +0800

    Add SiYuan icon (#1313)
    * Add SiYuan icon
    
    * Add SiYuan icon
    
    * Optimize icon color and size
    
    Co-authored-by: Philipp Kief <philipp.kief@gmx.de>

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -1728,6 +1728,7 @@ export const fileIcons: FileIcons = {
     },
     { name: 'rome', fileNames: ['rome.json'] },
     { name: 'cypress', fileNames: ['cypress.json', 'cypress.env.json'] },
+    { name: 'siyuan', fileExtensions: ['sy'] },
     { name: 'ndst', fileExtensions: ['ndst.yml', 'ndst.yaml', 'ndst.json'] },
   ],
 };
