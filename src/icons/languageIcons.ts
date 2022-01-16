72c7dee5832faaa7812458bed4a0e60dac4881d6
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-01-16 19:19:40 +0000

    add language icon
    Signed-off-by: Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>

--- src/icons/languageIcons.ts
+++ src/icons/languageIcons.ts
@@ -131,4 +131,5 @@ export const languageIcons: LanguageIcon[] = [
   { icon: { name: 'gemini' }, ids: ['gemini', 'text-gemini'] },
   { icon: { name: 'vlang' }, ids: ['v'] },
   { icon: { name: 'wolframlanguage' }, ids: ['wolfram'] },
+  { icon: { name: 'earthly' }, ids: ['Earthfile'] },
 ];
