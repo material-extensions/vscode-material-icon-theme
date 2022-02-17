7e7358356f1b5259e4de9d5eb4f72d46d3d6cd39
Ben Blackmore <ben.blackmore@steadybit.com>
2022-02-17 09:57:34 +0100

    feat: fileicon for steadybit configuration files

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -1771,5 +1771,15 @@ export const fileIcons: FileIcons = {
       name: 'gridsome',
       fileNames: ['gridsome.config.js', 'gridsome.server.js'],
     },
+    {
+      name: 'steadybit',
+      fileExtensions: ['steadybit.yml', 'steadybit.yaml'],
+      fileNames: [
+        '.steadybit.yml',
+        'steadybit.yml',
+        '.steadybit.yaml',
+        'steadybit.yaml',
+      ],
+    },
   ],
 };
