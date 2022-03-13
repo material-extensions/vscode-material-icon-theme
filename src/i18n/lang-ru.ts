e59f7abb4d40660151732da8ba3e9d0d1220ef61
Viktor <63919734+ViktorOn@users.noreply.github.com>
2022-03-13 21:54:29 +0200

    Update lang-ru.ts (#1427)

--- src/i18n/lang-ru.ts
+++ src/i18n/lang-ru.ts
@@ -7,7 +7,7 @@ export const translation: Translation = {
   activated: 'Material Icon Theme активен.',
   neverShowAgain: 'Никогда не показывать снова',
   themeUpdated: 'Material Icon Theme был обновлен.',
-  readChangelog: 'Читать изменения версии',
+  readChangelog: 'Смотреть список изменений',
   iconPacks: {
     selectPack: 'Выбрать набор иконок',
     description: "Выбрать '%0' набор иконок",
@@ -18,31 +18,31 @@ export const translation: Translation = {
     color: 'Выбрать цвет папки',
     hexCode: 'Вставить HEX-код цвета',
     wrongHexCode: 'Неверный HEX-код цвета!',
-    disabled: 'Нет иконки для папки',
+    disabled: 'Нет иконок для папки',
     theme: {
       description: "Выбрать '%0' тему папки",
     },
   },
   opacity: {
-    inputPlaceholder: 'Значение непрозрачности (от 0 до 1)',
-    wrongValue: 'Значение должно быть от 0 до 1!',
+    inputPlaceholder: 'Значение непрозрачности (между 0 до 1)',
+    wrongValue: 'Значение должно быть между 0 до 1!',
   },
   toggleSwitch: {
     on: 'Включить',
     off: 'Выключить',
   },
   explorerArrows: {
-    toggle: 'Показать/Скрыть стрелки у папок',
+    toggle: 'Показать/скрыть стрелки у папок',
     enable: 'Показать стрелки у папок',
     disable: 'Скрыть стрелки у папок',
   },
   confirmReload: 'Нужно перезапустить VS Code для активации иконок.',
   reload: 'Перезагрузить',
-  outdatedVersion: 'Нужно обновить VS Code чтобы использовать эту команду.',
+  outdatedVersion: 'Нужно обновить VS Code, чтобы использовать эту команду.',
   updateVSCode: 'Обновить VS Code',
   grayscale: {
     toggle: 'Переключение полутоновых значков',
-    enable: 'Включить полутоновые значки',
+    enable: 'Включить значки в оттенках серого',
     disable: 'Отключить значки в оттенках серого',
   },
   saturation: {
