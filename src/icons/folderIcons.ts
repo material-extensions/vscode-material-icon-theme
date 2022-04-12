6454dbaee663028b76122a512d2f929b5a272fb9
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-04-12 21:17:23 +0100

    Add folder icon for Robot Framework

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -743,6 +743,10 @@ export const folderIcons: FolderTheme[] = [
         name: 'folder-unity',
         folderNames: ['unity'],
       },
+      {
+        name: 'folder-robot',
+        folderNames: ['robot'],
+      },
     ],
   },
   {
