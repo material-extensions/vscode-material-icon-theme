fadd52cb4097afe0602000b0a93eb0045720d0a9
Bartłomiej Krakowski <48633090+bart-krakowski@users.noreply.github.com>
2022-07-04 22:10:28 +0200

    feat: jwt folder (#1566)
    * feat: jwt folder
    
    * fix: colors
    
    * fix: colors
    
    * fix: remove folder-jwt icon and associate jwt with folder-keys icon

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -565,7 +565,10 @@ export const folderIcons: FolderTheme[] = [
         folderNames: ['hook', 'hooks', 'trigger', 'triggers'],
       },
       { name: 'folder-job', folderNames: ['job', 'jobs'] },
-      { name: 'folder-keys', folderNames: ['keys', 'key', 'token', 'tokens'] },
+      {
+        name: 'folder-keys',
+        folderNames: ['keys', 'key', 'token', 'tokens', 'jwt'],
+      },
       { name: 'folder-layout', folderNames: ['layout', 'layouts', '_layouts'] },
       {
         name: 'folder-mail',
