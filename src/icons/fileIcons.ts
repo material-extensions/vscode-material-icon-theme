b8953849c0f119845400447db423969730e7c53c
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-07-05 20:10:38 +0000

    Add deno icon

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -1093,6 +1093,10 @@ export const fileIcons: FileIcons = {
       ],
     },
     {
+      name: 'deno',
+      fileNames: ['deno.json'],
+    },
+    {
       name: 'android',
       fileNames: ['androidmanifest.xml'],
       fileExtensions: ['apk', 'smali', 'dex'],
