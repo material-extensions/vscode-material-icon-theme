bcebb8dcc328d9ccd5d91cdabc42b7a90c345c2d
Sagiv Dayan <sagiv.keeper@gmail.com>
2022-10-18 11:33:22 +0300

    feat: add support for stylable

--- src/icons/languageIcons.ts
+++ src/icons/languageIcons.ts
@@ -137,4 +137,5 @@ export const languageIcons: LanguageIcon[] = [
   { icon: { name: 'svelte' }, ids: ['svelte'] },
   { icon: { name: 'dart' }, ids: ['dart'] },
   { icon: { name: 'cadence' }, ids: ['cadence'] },
+  { icon: { name: 'stylable' }, ids: ['stylable'] }
 ];
