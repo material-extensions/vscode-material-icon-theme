d68a0f4802182f807dbdc12ab335f0d27410ec04
Bartłomiej Krakowski <48633090+bart-krakowski@users.noreply.github.com>
2022-11-22 11:19:08 +0100

    Update src/icons/folderIcons.ts
    Co-authored-by: Maikel <maikel.van.dort@gmail.com>

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -576,7 +576,7 @@ export const folderIcons: FolderTheme[] = [
       },
       { name: 'folder-mappings', folderNames: ['mappings', 'mapping'] },
       { name: 'folder-meta', folderNames: ['meta'] },
-      { name: 'folder-changesets', folderNames: ['.changesets'] },
+      { name: 'folder-changesets', folderNames: ['.changesets', '.changeset'] },
       {
         name: 'folder-packages',
         folderNames: ['package', 'packages', 'pkg', 'pkgs'],
