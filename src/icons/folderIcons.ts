e26169e509b79efd9d756aa5561361bb659512db
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-12-07 10:50:42 +0100

    add storyblok folder icon

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -794,6 +794,10 @@ export const folderIcons: FolderTheme[] = [
         name: 'folder-proto',
         folderNames: ['protobufs', 'proto'],
       },
+      {
+        name: 'folder-storyblok',
+        folderNames: ['storyblok'],
+      },
     ],
   },
   {
