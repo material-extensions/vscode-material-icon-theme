354155eefeacf0a76e5e0188bc9962cb983ae427
Daomtthuan <dao.mt.thuan@gmail.com>
2022-12-25 02:10:45 +0700

    Add icon for .ruby-version

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -464,7 +464,11 @@ export const fileIcons: FileIcons = {
       ],
     },
     { name: 'lib', fileExtensions: ['lib', 'bib'] },
-    { name: 'ruby', fileExtensions: ['rb', 'erb'] },
+    {
+      name: 'ruby',
+      fileExtensions: ['rb', 'erb'],
+      fileNames: ['.ruby-version']
+    },
     { name: 'gemfile', fileNames: ['gemfile'] },
     {
       name: 'rubocop',
