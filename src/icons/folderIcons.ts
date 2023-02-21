d440af151981d124edce426a5c228df2c1f91d4b
Thaddeus Crews <repiteo@outlook.com>
2023-02-21 09:39:38 -0600

    Godot folder using optimized vector

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -807,6 +807,10 @@ export const folderIcons: FolderTheme[] = [
         name: 'folder-proto',
         folderNames: ['protobufs', 'proto'],
       },
+      {
+        name: 'folder-godot',
+        folderNames: ['godot', '.godot', 'godot-cpp'],
+      },
     ],
   },
   {
