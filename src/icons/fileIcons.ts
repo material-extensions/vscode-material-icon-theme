a62e2708a72e0ba4a995dd06638e021c265db99e
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2023-04-14 16:34:00 -0300

    feat(swagger): create folder and file icons

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -2324,5 +2324,6 @@ export const fileIcons: FileIcons = {
       ],
     },
     { name: 'typst', fileExtensions: ['typ'] },
+    { name: 'swagger', fileExtensions: ['swagger.ts', 'swagger.js', 'swagger.json'],  light: true,},
   ],
 };
--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -824,6 +824,10 @@ export const folderIcons: FolderTheme[] = [
         name: 'folder-godot',
         folderNames: ['godot', '.godot', 'godot-cpp', '.godot-cpp'],
       },
+      {
+        name: 'folder-swagger',
+        folderNames: ['swagger', '.swagger', 'Swagger'],
+      },
     ],
   },
   {
