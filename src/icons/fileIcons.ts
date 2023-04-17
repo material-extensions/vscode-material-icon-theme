cb01d2237a87e3900658e670aeabcf5fe3d355a6
LukeTimeWalker <lukas_zeitler@t-online.de>
2023-04-17 23:59:14 +0200

    add concourse icon

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -2325,5 +2325,6 @@ export const fileIcons: FileIcons = {
     },
     { name: 'typst', fileExtensions: ['typ'] },
     { name: 'ifanr-cloud', fileNames: ['.mincloudrc'] },
+    { name: 'concourse', fileNames: ['pipeline.yml'], light: true },
   ],
 };
