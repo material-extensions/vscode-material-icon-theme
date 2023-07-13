8c7ec38f9cefe7c09513df01a5de5d30da4f96d3
andyramseier <andy@megaphone.info>
2023-07-13 13:50:12 +0200

    add icons

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -829,6 +829,10 @@ export const folderIcons: FolderTheme[] = [
         name: 'folder-godot',
         folderNames: ['godot', '.godot', 'godot-cpp', '.godot-cpp'],
       },
+      {
+        name: 'folder-directives',
+        folderNames: ['directives'],
+      },
     ],
   },
   {
