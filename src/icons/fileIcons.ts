c1fe15038f84b72a1640a1237849c88799353fd9
Philipp Kief <philipp.kief@gmx.de>
2023-07-23 12:32:34 +0200

    [Icon Request] panda.config.ts Fixes PKief/vscode-material-icon-theme#2030

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -2398,5 +2398,6 @@ export const fileIcons: FileIcons = {
       ],
     },
     { name: 'roblox', fileExtensions: ['rbxl', 'rbxlx', 'rbxm', 'rbxmx'] },
+    { name: 'panda', fileNames: ['panda.config.ts', 'panda.config.js'] },
   ],
 };
