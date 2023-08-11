1db2900912a1acc9197700fd63540e8c831277c2
Ethosa <49402667+Ethosa@users.noreply.github.com>
2023-08-11 14:14:36 +0700

    add `hpx` file extension support

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -926,6 +926,7 @@ export const fileIcons: FileIcons = {
     { name: 'cake', fileExtensions: ['cake'] },
     { name: 'cucumber', fileExtensions: ['feature', 'features'] },
     { name: 'nim', fileExtensions: ['nim', 'nimble'] },
+    { name: 'hpx', fileExtensions: ['hpx'] },
     { name: 'apiblueprint', fileExtensions: ['apib', 'apiblueprint'] },
     { name: 'riot', fileExtensions: ['riot', 'tag'] },
     { name: 'vfl', fileExtensions: ['vfl'], fileNames: ['.vfl'] },
