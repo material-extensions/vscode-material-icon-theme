e8be32bd0ae5c854b87b15bc4577ea9f2e114de1
Philipp Kief <philipp.kief@gmx.de>
2023-10-03 11:14:36 +0200

    [Icon Request] ESBuild Fixes PKief/vscode-material-icon-theme#2087

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -2419,5 +2419,18 @@ export const fileIcons: FileIcons = {
         'panda.config.cjs',
       ],
     },
+    {
+      name: 'esbuild',
+      fileNames: [
+        'esbuild.js',
+        'esbuild.ts',
+        'esbuild.cjs',
+        'esbuild.mjs',
+        'esbuild.config.js',
+        'esbuild.config.ts',
+        'esbuild.config.cjs',
+        'esbuild.config.mjs',
+      ],
+    },
   ],
 };
