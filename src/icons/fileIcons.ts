ea3988a8e7bc31852dac837470be7a1182abd5b5
Philipp Kief <philipp.kief@gmx.de>
2023-12-23 11:41:48 +0100

    Use single color for icon

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -2422,7 +2422,7 @@ export const fileIcons: FileIcons = {
       ],
     },
     { name: 'ifanr-cloud', fileNames: ['.mincloudrc'] },
-    { name: 'concourse', fileNames: ['concourse.yml'], light: true },
+    { name: 'concourse', fileNames: ['concourse.yml'] },
     { name: 'qwik', fileExtensions: ['tsx'], enabledFor: [IconPack.Qwik] },
     { name: 'mermaid', fileExtensions: ['mmd', 'mermaid'] },
     {
