317acce092859a719555e37f8c65ddff07e26000
Jeffrey Ni <89946129+nijeff27@users.noreply.github.com>
2023-12-23 19:46:35 -0500

    Update fileIcons.ts

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -1968,7 +1968,9 @@ export const fileIcons: FileIcons = {
     { name: 'imba', fileExtensions: ['imba'] },
     { name: 'drawio', fileExtensions: ['drawio', 'dio'] },
     { name: 'pascal', fileExtensions: ['pas'] },
-    { name: 'shaderlab', fileExtensions: [
+    { 
+      name: 'shaderlab',
+      fileExtensions: [
         'animset',
         'asmdef',
         'asset',
