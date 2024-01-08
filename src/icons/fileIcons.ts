3803ba02d3e0a39f707bb85b5de7666fb7a4e28a
Philipp Kief <philipp.kief@gmx.de>
2024-01-08 11:51:21 +0100

    Adjust azure pipelines filename (#2186)
    * Adjust azure pipelines filename
    
    * Adjustment for yaml file extension

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -1728,8 +1728,18 @@ export const fileIcons: FileIcons = {
     },
     {
       name: 'azure-pipelines',
-      fileNames: ['azure-pipelines.yml', 'azure-pipelines.yaml'],
-      fileExtensions: ['azure-pipelines.yml', 'azure-pipelines.yaml'],
+      fileNames: [
+        'azure-pipelines.yml',
+        'azure-pipelines.yaml',
+        'azure-pipelines-main.yml',
+        'azure-pipelines-main.yaml',
+      ],
+      fileExtensions: [
+        'azure-pipelines.yml',
+        'azure-pipelines.yaml',
+        'azure-pipelines-main.yml',
+        'azure-pipelines-main.yaml',
+      ],
     },
     { name: 'azure', fileExtensions: ['azcli'] },
     {
