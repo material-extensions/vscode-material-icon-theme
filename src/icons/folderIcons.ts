3c4f108c37853fc1540f3cdb739b89e735d2b5c6
Gabriel Dugny <git@dugny.me>
2024-01-30 10:32:43 +0100

    chore: add .pdm-build

--- src/icons/folderIcons.ts
+++ src/icons/folderIcons.ts
@@ -88,7 +88,7 @@ export const folderIcons: FolderTheme[] = [
         light: true,
       },
       { name: 'folder-markdown', folderNames: ['markdown', 'md'] },
-      { name: 'folder-pdm', folderNames: ['.pdm-plugins'] },
+      { name: 'folder-pdm', folderNames: ['.pdm-plugins', '.pdm-build'] },
       { name: 'folder-php', folderNames: ['php'] },
       { name: 'folder-phpmailer', folderNames: ['phpmailer'] },
       { name: 'folder-sublime', folderNames: ['sublime'] },
