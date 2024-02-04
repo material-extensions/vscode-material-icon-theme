1a4c7c1f9e81919cce07230c9e40810ae1a6a2b3
Lucas Colombo <lucasncolombo@gmail.com>
2024-02-04 07:02:11 -0300

    ci: 🔄 add addon icon folder to icon review gh action

--- src/lucodear/generator.ts
+++ src/lucodear/generator.ts
@@ -1,11 +1,11 @@
-import { FolderTheme, IconConfiguration, IconJsonOptions } from '../models';
+import { IconConfiguration, IconJsonOptions } from '../models';
 import merge from 'lodash.merge';
 /**
  * Get addons icon definitions as object.
  */
 export const loadLucodearAddonIconDefinitions = (
   config: IconConfiguration,
-  options: IconJsonOptions
+  _options: IconJsonOptions
 ): IconConfiguration => {
   config = merge({}, config);
   // TODO :)
