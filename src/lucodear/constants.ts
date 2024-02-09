9b1bac4bc206f1d912ff091ce4115bf36e6c44fa
Lucas Colombo <lucasncolombo@gmail.com>
2024-02-09 01:27:55 -0300

    ci: 🔄 fix ci action routes

--- src/lucodear/constants.ts
+++ src/lucodear/constants.ts
@@ -1,3 +1,1 @@
-// export const lucodearFileIconsPath: string = './../icons-lucodear/files/';
-// export const lucodearFolderIconsPath: string = './../icons-lucodear/folders/';
 export const lucodearIconsPath: string = './../icons-lc/';
