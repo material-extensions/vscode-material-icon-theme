aec9cc1d00148993625085cec820fe2b43cec90b
Lucas Colombo <lucasncolombo@gmail.com>
2024-02-20 03:47:03 -0300

    feat: ✨ lucodear: lucode config files

--- src/lucodear/icons/lucodear/files.ts
+++ src/lucodear/icons/lucodear/files.ts
@@ -1,6 +1,11 @@
 import { LucodearFileIcon } from '../../model';
 import { lucodear } from '../utils';
 
+const nameAndExtension = (...files: string[]) => ({
+  fileExtensions: files,
+  fileNames: files,
+});
+
 export const files = lucodear('lucodear', [
   {
     name: 'coco',
@@ -22,5 +27,43 @@ export const files = lucodear('lucodear', [
       'kurvrc.yaml',
       'kurvconf.yaml',
     ],
+  },
+  {
+    name: 'lucode',
+    fileNames: [
+      'lucode.toml',
+      'lucoderc.toml',
+      'lc.toml',
+      'lucodear.toml',
+      'lucodearrc.toml',
+    ],
+    fileExtensions: ['lucode', 'lucoderc', 'lc', 'lucodear', 'lucodearrc'],
+  },
+  {
+    name: 'lucode-json',
+    ...nameAndExtension(
+      'lucode.json',
+      'lucode.jsonc',
+      'lucoderc.json',
+      'lucoderc.jsonc',
+      'lc.json',
+      'lc.jsonc',
+      'lucodear.json',
+      'lucodear.jsonc'
+    ),
+    light: true,
+  },
+  {
+    name: 'lucode-yaml',
+    ...nameAndExtension(
+      'lucode.yml',
+      'lucode.yaml',
+      'lucoderc.yml',
+      'lucoderc.yaml',
+      'lc.yml',
+      'lc.yaml',
+      'lucodear.yml',
+      'lucodear.yaml'
+    ),
   },
 ] as LucodearFileIcon[]);
