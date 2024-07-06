3e6fa05e9a6f3799147dced084587e74c6934336
Okinea Dev <81070564+okineadev@users.noreply.github.com>
2024-07-06 14:31:12 +0200

    Added cairo icon

--- src/icons/fileIcons.ts
+++ src/icons/fileIcons.ts
@@ -2460,5 +2460,9 @@ export const fileIcons: FileIcons = {
       name: 'bruno',
       fileExtensions: ['bru'],
     },
+    {
+      name: 'cairo',
+      fileExtensions: ['cairo'],
+    },
   ]),
 };
