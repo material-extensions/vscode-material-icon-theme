021a82dced4f1dfe7a881dca385ee4e59ed96dc5
Philipp Kief <philipp.kief@gmx.de>
2024-07-13 13:38:19 +0200

    Update customIconPaths

--- src/core/helpers/customIconPaths.ts
+++ src/core/helpers/customIconPaths.ts
@@ -1,13 +1,10 @@
-import { dirname, join } from 'node:path';
+import { dirname } from 'node:path';
+import { resolvePath } from './resolvePath';
 
 export const getCustomIconPaths = (
   filesAssociations: Record<string, string> = {}
 ) => {
   return Object.values(filesAssociations)
     .filter((fileName) => fileName.match(/^[.\/]+/)) // <- custom dirs have a relative path to the dist folder
-    .map((fileName) => {
-      // go up two directories to get to the .vscode/extensions/icons folder where the custom icons are stored
-      // Only working when the filename is a relative path that looks like this: '../../icons/<filename>'
-      return dirname(join(__dirname, '..', '..', fileName));
-    });
+    .map((fileName) => dirname(resolvePath(fileName)));
 };
