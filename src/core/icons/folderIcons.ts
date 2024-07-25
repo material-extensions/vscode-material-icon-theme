96e1605df01a8f4b7381dea5380701eac133e4bc
Okinea Dev <81070564+okineadev@users.noreply.github.com>
2024-07-25 21:50:04 +0300

    Added `ipynb` folder icon (#2479)

--- src/core/icons/folderIcons.ts
+++ src/core/icons/folderIcons.ts
@@ -750,7 +750,7 @@ export const folderIcons: FolderTheme[] = [
       },
       {
         name: 'folder-jupyter',
-        folderNames: ['jupyter', 'notebook', 'notebooks'],
+        folderNames: ['jupyter', 'notebook', 'notebooks', 'ipynb'],
       },
       { name: 'folder-scala', folderNames: ['scala'] },
       {
