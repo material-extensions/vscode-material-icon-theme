c68d8626382720580f8e3ab6162d62d45579f161
Philipp Kief <philipp.kief@gmx.de>
2024-11-06 10:16:01 +0100

    Adjust commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,9 +4,10 @@
 const config = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'header-max-length': () => [0, 'always', 72],
     'subject-case': [
-      2,
-      'never',
+      0,
+      'always',
       ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
     ],
   },
