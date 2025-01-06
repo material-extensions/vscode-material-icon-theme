f385cf8bc9cf49c9369a42cdc42675f0bf9c28fa
Okinea Dev <hi@okinea.dev>
2025-01-06 13:52:48 +0200

    feat(ci): ✨ improve labeler script

--- .github/actions/update-status-label.sh
+++ .github/actions/update-status-label.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 # Only for GitHub Actions
+# Parameters: <pr_number> <status> <label>
+# Example: ./update-status-label.sh 1234 success "ready-for-review"
 
+# Exit on error
 set -e
 
 pr_number="$1"
 status="$2"
 label="$3"
 
-if [ "$status" == "failure" ]; then
-    gh pr edit "$pr_number" --add-label "$label"
-else
-    gh pr edit "$pr_number" --remove-label "$label"
-fi
+action="add-label" && [ "$status" == "success" ] && action="remove-label"
+
+# Add or remove label
+gh pr edit "$pr_number" --${action} "$label"
