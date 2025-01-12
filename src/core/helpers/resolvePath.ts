9788e1040a5fa376e43fca42cf2a7f274063f191
Philipp Kief <philipp.kief@gmx.de>
2025-01-12 19:16:17 +0100

    Refactor resolve path function

--- src/core/helpers/resolvePath.ts
+++ src/core/helpers/resolvePath.ts
@@ -1,5 +1,11 @@
 import { join } from 'node:path';
 
-export const resolvePath = (path: string): string => {
-  return join(__dirname, '..', '..', path);
+/**
+ * Resolves a sequence of path segments into an absolute path.
+ *
+ * @param paths - A list of path segments to be joined and resolved relative to the module's root directory.
+ * @returns The resolved absolute path as a string.
+ */
+export const resolvePath = (...paths: string[]): string => {
+  return join(__dirname, '..', '..', ...paths);
 };
