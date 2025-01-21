8533afecbdf009203549a8d6054867e18cfc5a43
Okinea Dev <hi@okinea.dev>
2025-01-21 16:45:39 +0200

    feat: expand the list of file extensions for the `email` icon
    * Expanded the `email` icon configuration to include support for additional file extensions such as `.edb`, `.emlx`, `.mbox`, `.oft`, `.olm`, `.ost`, `.p7s`, `.pst`, `.rpmsg`, and `.tnef`
    
    Source: https://docs.fileformat.com/email/

--- src/core/icons/fileIcons.ts
+++ src/core/icons/fileIcons.ts
@@ -785,7 +785,25 @@ export const fileIcons: FileIcons = {
     },
     { name: 'virtual', fileExtensions: ['vdi', 'vbox', 'vbox-prev'] },
     { name: 'vedic', fileExtensions: ['ved', 'veda', 'vedic'] },
-    { name: 'email', fileExtensions: ['ics'], fileNames: ['.mailmap'] },
+    {
+      name: 'email',
+      fileExtensions: [
+        'edb',
+        'eml',
+        'emlx',
+        'ics',
+        'mbox',
+        'msg',
+        'oft',
+        'olm',
+        'ost',
+        'p7s',
+        'pst',
+        'rpmsg',
+        'tnef',
+      ],
+      fileNames: ['.mailmap'],
+    },
     {
       name: 'audio',
       fileExtensions: ['mp3', 'flac', 'm4a', 'wma', 'aiff', 'wav'],
@@ -2946,10 +2964,6 @@ export const fileIcons: FileIcons = {
     {
       name: 'wrangler',
       fileNames: ['wrangler.toml'],
-    },
-    {
-      name: 'mail',
-      fileExtensions: ['eml', 'msg'],
     },
   ]),
 };
