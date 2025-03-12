338180a7408568fa601ebc9712d69661cbbefa4b
Cameron Cowen <cameronc94@gmail.com>
2025-03-12 00:37:08 +0000

    added folder icons for kusto

--- src/core/icons/folderIcons.ts
+++ src/core/icons/folderIcons.ts
@@ -1132,6 +1132,7 @@ export const folderIcons: FolderTheme[] = [
       { name: 'folder-liquibase', folderNames: ['liquibase'] },
       { name: 'folder-dart', folderNames: ['dart', 'dart_tool', 'dart_tools'] },
       { name: 'folder-zeabur', folderNames: ['zeabur'] },
+      { name: 'folder-kusto', folderNames: ['kusto', 'kql'] },
     ],
   },
   {
