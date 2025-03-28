577ca60bfb16ba63caa7db0b76ccc6a1a413382b
gtkacz <gmtkacz@proton.me>
2025-03-28 17:52:26 -0300

    feat: add support for more Python-adjacent file types

--- src/core/icons/fileIcons.ts
+++ src/core/icons/fileIcons.ts
@@ -488,7 +488,7 @@ export const fileIcons: FileIcons = {
     { name: 'python', fileExtensions: ['py'] },
     {
       name: 'python-misc',
-      fileExtensions: ['pyc', 'whl'],
+      fileExtensions: ['pyc', 'whl', 'egg'],
       fileNames: [
         'requirements.txt',
         'pipfile',
