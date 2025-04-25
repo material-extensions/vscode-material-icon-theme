e941a1b18f1f3ea40f5d211bcf1914dd9453d22d
Marlon Luís de Col <marlondecol@proton.me>
2025-04-25 18:24:38 +0000

    feat: recreate different icons for hooks and triggers folders

--- src/core/icons/folderIcons.ts
+++ src/core/icons/folderIcons.ts
@@ -734,7 +734,11 @@ export const folderIcons: FolderTheme[] = [
       },
       {
         name: 'folder-hook',
-        folderNames: ['hook', 'hooks', 'trigger', 'triggers'],
+        folderNames: ['hook', 'hooks'],
+      },
+      {
+        name: 'folder-trigger',
+        folderNames: ['trigger', 'triggers'],
       },
       { name: 'folder-job', folderNames: ['job', 'jobs'] },
       {
