5fc78fe2746878924be9f7e61d845ce127dd2801
Marlon Luís de Col <marlondecol@proton.me>
2025-04-30 21:19:41 -0300

    feat: add icon for bibliographies folder

--- src/core/icons/folderIcons.ts
+++ src/core/icons/folderIcons.ts
@@ -1167,6 +1167,10 @@ export const folderIcons: FolderTheme[] = [
       { name: 'folder-kusto', folderNames: ['kusto', 'kql'] },
       { name: 'folder-policy', folderNames: ['policy', 'policies'] },
       { name: 'folder-attachment', folderNames: ['attachment', 'attachments'] },
+      {
+        name: 'folder-bibliography',
+        folderNames: ['bibliography', 'bibliographies', 'book', 'books'],
+      },
     ],
   },
   {
