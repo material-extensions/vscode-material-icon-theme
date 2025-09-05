cb8d402fa38c762f34b5c5a504267c878cef1345
Icedsoda <29167146+1cedsoda@users.noreply.github.com>
2025-09-05 08:59:17 +0200

    feat: add onnx icon (#3160)
    Co-authored-by: Philipp Kief <philipp.kief@gmx.de>

--- src/core/icons/fileIcons.ts
+++ src/core/icons/fileIcons.ts
@@ -1075,6 +1075,7 @@ export const fileIcons: FileIcons = {
     },
     { name: 'ocaml', fileExtensions: ['ml', 'mli', 'cmx'] },
     { name: 'odin', fileExtensions: ['odin'] },
+    { name: 'onnx', fileExtensions: ['onnx'] },
     {
       name: 'javascript-map',
       fileExtensions: ['js.map', 'mjs.map', 'cjs.map'],
