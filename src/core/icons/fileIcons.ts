ac4ee0f8486dd88b0415966057486538d5270aa1
ViniDevBR <vinigtr386@gmail.com>
2025-09-08 16:53:25 -0300

    feat: new google icon file

--- src/core/icons/fileIcons.ts
+++ src/core/icons/fileIcons.ts
@@ -3283,7 +3283,12 @@ export const fileIcons: FileIcons = {
     },
     {
       name: 'metro',
-      fileNames: ['metro.config.js', 'metro.config.json'],
+      fileNames: [
+        'metro.config.js',
+        'metro.config.cjs',
+        'metro.config.mjs',
+        'metro.config.json',
+      ],
     },
     {
       name: 'bashly',
@@ -3320,6 +3325,10 @@ export const fileIcons: FileIcons = {
       name: 'bashly-hook',
       fileNames: ['src/initialize.sh', 'src/before.sh', 'src/after.sh'],
       enabledFor: [IconPack.Bashly],
+    },
+    {
+      name: 'google',
+      fileNames: ['google-services.json', 'GoogleService-Info.plist'],
     },
   ]),
 };
