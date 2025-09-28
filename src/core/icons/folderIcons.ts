f82273d67783e45ecafd5e842316a1de170c648d
dmotte <37443982+dmotte@users.noreply.github.com>
2025-09-28 13:16:31 +0200

    feat: use folder-svg icon for folder name 'vector'

--- src/core/icons/folderIcons.ts
+++ src/core/icons/folderIcons.ts
@@ -977,7 +977,7 @@ export const folderIcons: FolderTheme[] = [
         name: 'folder-interceptor',
         folderNames: ['interceptor', 'interceptors'],
       },
-      { name: 'folder-svg', folderNames: ['svg', 'svgs'] },
+      { name: 'folder-svg', folderNames: ['svg', 'svgs', 'vector'] },
       {
         name: 'folder-vuex-store',
         folderNames: ['store', 'stores'],
