a5b98881911cfed027df5d4cb45e7aa47ce66e62
Alex Dombroski <alexdombroski1@gmail.com>
2025-11-21 23:07:13 -0700

    add devops folder svg mapping

--- src/core/icons/folderIcons.ts
+++ src/core/icons/folderIcons.ts
@@ -523,6 +523,10 @@ export const folderIcons: FolderTheme[] = [
       },
       { name: 'folder-ci', folderNames: ['ci'] },
       {
+        name: 'folder-devops',
+        folderNames: ['devops'],
+      },
+      {
         name: 'folder-eslint',
         folderNames: [
           'eslint',
