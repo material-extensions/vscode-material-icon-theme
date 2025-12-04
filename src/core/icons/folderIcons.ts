03b00c6b4142eb858818af3b17968cb5b1d14b92
Roman <me@rman.dev>
2025-12-04 18:28:49 +0000

    [wip] actions folder

--- src/core/icons/folderIcons.ts
+++ src/core/icons/folderIcons.ts
@@ -1333,6 +1333,10 @@ export const folderIcons: FolderTheme[] = [
         name: 'folder-license',
         folderNames: ['license', 'licenses'],
       },
+      {
+        name: 'folder-actions',
+        folderNames: ['actions'],
+      }
     ],
   },
   {
