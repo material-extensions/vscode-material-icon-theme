7d2056efd74da5f7d30321cc207f312643050461
Philipp Kief <philipp.kief@gmx.de>
2026-02-22 11:37:03 +0100

    feat(icon): replace .toon_light icon with .toon icon and update fileIcons (#3365)

--- src/core/icons/fileIcons.ts
+++ src/core/icons/fileIcons.ts
@@ -113,7 +113,7 @@ export const fileIcons: FileIcons = {
       fileNames: ['.htaccess'],
     },
     { name: 'toml', fileExtensions: ['toml'], light: true },
-    { name: 'toon', fileExtensions: ['toon'], light: true },
+    { name: 'toon', fileExtensions: ['toon'] },
     {
       name: 'image',
       fileExtensions: [
