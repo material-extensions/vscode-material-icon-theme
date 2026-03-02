f1d5832e9101f4acc41af6e7683c31897484dd17
Cameron Cowen <cameronc94@gmail.com>
2026-03-02 23:11:08 +0000

    initial postman folder icons

--- src/core/icons/folderIcons.ts
+++ src/core/icons/folderIcons.ts
@@ -1370,6 +1370,10 @@ export const folderIcons: FolderTheme[] = [
         name: 'folder-form',
         folderNames: ['form', 'forms'],
       },
+      {
+        name: 'folder-postman',
+        folderNames: ['postman'],
+      },
     ],
   },
   {
