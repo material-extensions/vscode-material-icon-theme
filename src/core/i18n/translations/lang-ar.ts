c92dba57e3803c31b16d2bcaa08b8621ea6c3fc2
FireMoonStudio <firemoongamesstudio@gmail.com>
2026-03-16 22:44:43 +0300

    Add files via upload
    Signed-off-by: FireMoonStudio <firemoongamesstudio@gmail.com>

---
+++ src/core/i18n/translations/lang-ar.ts
@@ -0,0 +1,46 @@
+import type { Translation } from '../../models/i18n/translation';
+
+export const translation: Translation = {
+  activate: 'تفعيل',
+  activated: 'تم تفعيل Material Icon Theme.',
+  iconPacks: {
+    selectPack: 'اختر حزمة الأيقونات',
+    description: "اختر حزمة الأيقونات '%0'",
+    disabled: 'تعطيل حزم الأيقونات',
+  },
+  folders: {
+    toggleIcons: 'اختر سمة المجلد',
+    disabled: 'بدون أيقونات مجلدات',
+    theme: {
+      description: "اختر سمة المجلد '%0'",
+    },
+  },
+  colorSelect: {
+    color: 'اختر لونًا',
+    hexCode: 'أدخل كود لون HEX',
+    wrongHexCode: 'كود HEX غير صالح!',
+  },
+  opacity: {
+    inputPlaceholder: 'قيمة الشفافية (بين 0 و 1)',
+    wrongValue: 'يرجى إدخال رقم عشري بين 0 و 1.',
+  },
+  toggleSwitch: {
+    on: 'تشغيل',
+    off: 'إيقاف',
+  },
+  explorerArrows: {
+    toggle: 'تبديل أسهم المجلدات في المستكشف',
+    enable: 'إظهار أسهم المجلدات في المستكشف',
+    disable: 'إخفاء أسهم المجلدات في المستكشف',
+  },
+  grayscale: {
+    toggle: 'تبديل الأيقونات الرمادية',
+    enable: 'تفعيل الأيقونات الرمادية',
+    disable: 'تعطيل الأيقونات الرمادية',
+  },
+  saturation: {
+    inputPlaceholder: 'قيمة التشبع (بين 0 و 1)',
+    wrongValue: 'يرجى إدخال رقم عشري بين 0 و 1.',
+  },
+
+};
