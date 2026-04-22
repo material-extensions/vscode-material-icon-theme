12ade7a393f2d0b0666b384c8e65c6d0209059d4
Sayan Shankhari <sayan0shankhari@gmail.com>
2026-04-22 15:33:30 +0530

    feat(icon): add internal folder icon
    Resolves: #3328

--- src/core/icons/folderIcons.ts
+++ src/core/icons/folderIcons.ts
@@ -1415,6 +1415,7 @@ export const folderIcons: FolderTheme[] = [
       { name: 'folder-appwrite', folderNames: ['appwrite'] },
       { name: 'folder-assembly', folderNames: ['assembly', 'asm'] },
       { name: 'folder-go', folderNames: ['go', 'golang'] },
+      { name: 'folder-internal', folderNames: ['internal'] },
     ],
   },
   {
