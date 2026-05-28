263b329b45a5b4f8116c2c4f661e8910cffcc0d5
Young <young20050727@gmail.com>
2026-05-29 00:42:35 +0800

    Add hip file extension icon mapping

--- src/core/icons/fileIcons.ts
+++ src/core/icons/fileIcons.ts
@@ -616,6 +616,7 @@ export const fileIcons: FileIcons = {
     { name: 'c3', fileExtensions: ['c3'] },
     { name: 'c', fileExtensions: ['c', 'i', 'mi'] },
     { name: 'h', fileExtensions: ['h'] },
+    { name: 'hip', fileExtensions: ['hip'] },
     {
       name: 'cpp',
       fileExtensions: [
