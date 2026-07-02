28fcd1504bdbfad914a21998f5c5dd8b1bceb027
Padraic Slattery <pgoslatara@gmail.com>
2026-07-02 10:54:37 +0200

    Adding YAML file support

--- src/core/icons/fileIcons.ts
+++ src/core/icons/fileIcons.ts
@@ -550,7 +550,7 @@ export const fileIcons: FileIcons = {
         'dsql',
       ],
     },
-    { name: 'dbt-bouncer', fileNames: ['dbt-bouncer.yml', 'dbt-bouncer.toml'] },
+    { name: 'dbt-bouncer', fileNames: ['dbt-bouncer.yml', 'dbt-bouncer.yaml','dbt-bouncer.toml'] },
     { name: 'kusto', fileExtensions: ['kql'] },
     { name: 'csharp', fileExtensions: ['cs', 'csx', 'csharp'] },
     { name: 'qsharp', fileExtensions: ['qs'] },
