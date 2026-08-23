7f109357026a639f822ddd57c248cd8aea561f73
Philipp Kief <philipp.kief@gmx.de>
2026-08-23 15:25:39 +0200

    fix: resolve TypeScript indexing error in deepEqual

--- src/extension/tools/utils/deepEqual.ts
+++ src/extension/tools/utils/deepEqual.ts
@@ -47,18 +47,21 @@ export function deepEqual(a: unknown, b: unknown): boolean {
     return false;
   }
 
-  const keysA = Object.keys(a);
-  const keysB = Object.keys(b);
+  const objA = a as Record<string, unknown>;
+  const objB = b as Record<string, unknown>;
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
 
   if (keysA.length !== keysB.length) {
     return false;
   }
 
   for (const key of keysA) {
-    if (!Object.prototype.hasOwnProperty.call(b, key)) {
+    if (!Object.prototype.hasOwnProperty.call(objB, key)) {
       return false;
     }
-    if (!deepEqual(a[key], b[key])) {
+    if (!deepEqual(objA[key], objB[key])) {
       return false;
     }
   }
