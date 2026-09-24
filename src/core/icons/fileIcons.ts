78b11c13467fe7259973962e23b835682c48fed8
Cameron Cowen <cameronc94@gmail.com>
2026-09-24 20:28:54 +0100

    formatting

--- src/core/icons/fileIcons.ts
+++ src/core/icons/fileIcons.ts
@@ -3799,7 +3799,7 @@ export const fileIcons: FileIcons = {
     },
     {
       name: 'zizmor',
-      fileExtensions: ['zizmor.yml', 'zizmor.yaml']
-    }
+      fileExtensions: ['zizmor.yml', 'zizmor.yaml'],
+    },
   ]),
 };
